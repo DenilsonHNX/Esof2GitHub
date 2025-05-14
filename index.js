5be87a50581a520c2451017b74d7dd6e4fff9784
DenilsonHNX <denilsonultralink1@gmail.com>
2025-05-14 08:54:30 +0100

    Retirando texto de conflito

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 console.log("Hello World!");
-<<<<<<< HEAD
 function soma(var a, var b){
 	return a+b;
-=======
-
+}
 function sum(var num1, var num2)
 {
 	return num1+num2;
->>>>>>> a9b862d36b549b31d77b1c36d734ac29ddfccb5d
+
 }
